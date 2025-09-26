f0154cabcd4bb6b32317939958d57e50174f525e
uendi-lab <uendi2000@gmail.com>
2025-09-26 20:02:22 +0200

    Aggiunta progetti personali e adeguamento API e stile

---
+++ cms-architetto/src/api/progetti-personali/controllers/progetti-personali.ts
@@ -0,0 +1,7 @@
+/**
+ * progetti-personali controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::progetti-personali.progetti-personali');
---
+++ cms-architetto/src/api/progetti-personali/services/progetti-personali.ts
@@ -0,0 +1,7 @@
+/**
+ * progetti-personali service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::progetti-personali.progetti-personali');
