d12b05c4e9ba3dfc9bb90dc8dda96349a618905d
uendi-lab <uendi2000@gmail.com>
2026-05-04 21:45:07 +0200

    commit

--- cms-architetto/config/plugins.ts
+++ cms-architetto/config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+          user: 'uendi2000@gmail.com',
+          pass: 'iyjc ktxm efll hpdl',
+        },
+      },
+      settings: {
+        defaultFrom: 'uendi2000@gmail.com',
+        defaultReplyTo: 'uendi2000@gmail.com',
+      },
+    },
+  },
+});
--- cms-architetto/src/index.ts
+++ cms-architetto/src/index.ts
@@ -1,20 +1,67 @@
-// import type { Core } from '@strapi/strapi';
-
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register() {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }: any) {
+    strapi.documents.use(async (context: any, next: any) => {
+      const result = await next();
+
+      if (
+        context.uid === 'api::contact.contact' &&
+        context.action === 'create'
+      ) {
+        try {
+          const data = result;
+
+          console.log('NUOVO CONTACT:', data);
+          
+          await strapi.plugins['email'].services.email.send({
+  to: 'uendi2000@gmail.com',
+  subject: `Nuovo messaggio da ${data.nome}`,
+  text:
+`Nuovo messaggio dal sito
+
+Nome: ${data.nome}
+Email: ${data.email}
+Oggetto: ${data.oggetto}
+
+Messaggio:
+${data.messaggio}`,
+
+  html: `
+    <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+      <h2>Nuovo messaggio dal sito</h2>
+
+      <p><strong>Nome:</strong> ${data.nome}</p>
+      <p><strong>Email:</strong> ${data.email}</p>
+      <p><strong>Oggetto:</strong> ${data.oggetto}</p>
+
+      <hr style="border: none; border-top: 1px solid #ddd; margin: 20px 0;" />
+
+      <p><strong>Messaggio:</strong></p>
+      <p>${data.messaggio}</p>
+    </div>
+  `,
+});
+   //       await strapi.plugins['email'].services.email.send({
+    //        to: 'uendi2000@gmail.com',
+      //      subject: `Nuovo messaggio da ${data.nome}`,
+        //    text: `
+//Nome: ${data.nome}
+//Email: ${data.email}
+//Oggetto: ${data.oggetto}
+
+//Messaggio:
+//${data.messaggio}
+  //          `,
+    //      });
+
+          console.log('EMAIL INVIATA');
+        } catch (error) {
+          console.error('ERRORE EMAIL:', error);
+        }
+      }
+
+      return result;
+    });
+  },
 };
